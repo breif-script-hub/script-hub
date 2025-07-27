2004731310036bfb5a7ec119cce8d088ba57cd13
thebiraf <raffertychambers@gmail.com>
2025-07-27 11:51:55 +0100

    Update scripthub.lua

--- scripthub.lua
+++ scripthub.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 81440632616906 then
     pcall(function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/4HBjnPzx"))()
+        loadstring(game:HttpGet("https://pastebin.com/raw/5NHqnmJ6"))()
     end)
 end
