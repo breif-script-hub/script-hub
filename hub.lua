f94a335b14d46f445fcbc4faa1ce20c893ada3b1
thebiraf <raffertychambers@gmail.com>
2025-07-28 09:28:47 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -9,3 +9,8 @@ if game.PlaceId == 17625359962 or game.PlaceId == 117398147513099 then
         loadstring(game:HttpGet("https://pastebin.com/raw/LVGpyNMS"))()
     end)
 end
+if game.PlaceId == 13355910112 then
+    pcall(function()
+        loadstring(game:HttpGet("https://pastebin.com/raw/FXtjAeWm"))()
+    end)
+end
