82dff1b51266d0644015fa57dce7bb85acdb1450
thebiraf <raffertychambers@gmail.com>
2025-08-03 14:15:29 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -1,3 +1,4 @@
+-- anti afk
 print ("anti afk activated")
 task.spawn(function()
     local vu = game:GetService("VirtualUser")
@@ -13,24 +14,27 @@ if game.PlaceId == 81440632616906 then
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/078b24d6aea94f58fc97a21e683abbfa/raw/d53813b1c974f40d8016fadbce977304b4f46dec/dig%2520to%2520earths%2520core"))()
     end)
-end
-
-
-if game.PlaceId == 13355910112 then
+elseif game.PlaceId == 13355910112 then
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/7205d43e2910940049de839dde5c9392/raw/cd46d41427c6ea78c8dfb2b5556460aebe7fe1bd/every%2520seacond%2520you%2520get%2520faster"))()
     end)
-end
-
-if game.PlaceId == 5256165620 then
+elseif game.PlaceId == 5256165620 then
     pcall(function()
-        loadstring(game:HttpGet("https://pastebin.com/raw/MdA6jZvT"))()
+        loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/66efd9c34cf21884e9e1e3264388cbfc/raw/dc6f587f56db5d301439770c91630dfda7b2797e/3%25202%25201%2520blast%2520off"))()
     end)
-end
-
--- new place
-if game.PlaceId == 75663528075786 then
+elseif game.PlaceId == 75663528075786 then
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/32dfb4ed746c5d54cd2b031356666476/raw/224c41cc309b621cd2a972e8119c083a5facd254/How%2520to%2520train%2520ur%2520dragon"))()
+    end)
+-- future scripts
+elseif game.PlaceId ==  then
+    pcall(function()
+        loadstring(game:HttpGet(""))()
+    end)
+-- if game isnt supported
+else
+    pcall(function()
+        print("Youru are not in a supported game")
+        loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/0c9a5657c4403429ba107179fb2f356b/raw/6edc39eb06e01b538e7d92f8c4f18a663280d9df/Unsupported%2520game"))()
     end)
 end
