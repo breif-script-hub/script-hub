0cc20a9959a88025526ad1a900cc6f1622390670
thebiraf <raffertychambers@gmail.com>
2025-08-14 13:52:58 +0100

    Update hub.lua

--- hub.lua
+++ hub.lua
@@ -10,33 +10,37 @@ task.spawn(function()
 end)
 
 -- Main 
-if game.PlaceId == 81440632616906 then
+if game.PlaceId == 81440632616906 then -- dig to earths core
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/078b24d6aea94f58fc97a21e683abbfa/raw/d53813b1c974f40d8016fadbce977304b4f46dec/dig%2520to%2520earths%2520core"))()
     end)
-elseif game.PlaceId == 13355910112 then
+elseif game.PlaceId == 13355910112 then -- every seacond you get faster
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/7ccd216eeac9ee67a028ad76a7c08dc4/raw/7076596495adb50d5ce48c2531636fa1f7a4ea29/every%2520seacond%2520you%2520get%2520faster"))()
     end)
-elseif game.PlaceId == 5256165620 then
+elseif game.PlaceId == 5256165620 then -- 3 2 1 blast off simulator
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/66efd9c34cf21884e9e1e3264388cbfc/raw/dc6f587f56db5d301439770c91630dfda7b2797e/3%25202%25201%2520blast%2520off"))()
     end)
-elseif game.PlaceId == 75663528075786 then
+elseif game.PlaceId == 75663528075786 then -- how to train ur dragon (barely supported)
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/32dfb4ed746c5d54cd2b031356666476/raw/224c41cc309b621cd2a972e8119c083a5facd254/How%2520to%2520train%2520ur%2520dragon"))()
     end)
-elseif game.PlaceId == 13461044782 then
+elseif game.PlaceId == 13461044782 then -- every seacond you jump higher
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/947737c51b4716a23a6e61b1d1c3dd85/raw/943c4e0d062e7c54fe2b0da29380feced3a66206/every%2520seacond%2520you%2520get%2520higher%2520jump%2520power"))()
     end)
 
-elseif game.PlaceId == 13501167264 then
+elseif game.PlaceId == 13501167264 then -- build a raft or die
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/614354c746ebfadeb757e9a1fffe7f5c/raw/2c385358ac700218aa4004461ef70d22f5bbba1d/build%2520a%2520raft%2520or%2520die"))()
     end)
+elseif game.PlaceId == 14218711698  then -- chriminal tycoon
+    pcall(function()
+        loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/8141ba0db0f950a21a8c3a597ba46aff/raw/ee88fcca9ab0bc498a70129a683f6507166729a4/chriminal%2520tycoon"))()
+    end)
 -- Example code for future updates
-elseif game.PlaceId == 0 then
+elseif game.PlaceId == 0 then -- example
     pcall(function()
         loadstring(game:HttpGet("https://gist.githubusercontent.com/thebiraf/451324b8baeaa0f0ca437e86f9811530/raw/db09f4f2d2318c2d1dff0a214303eee9628ab921/invalid"))()
     end)
